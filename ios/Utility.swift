9dd5b7c7939cde7a87f4876641b658acef461452
Manikandan <manikandan707@gmail.com>
2024-02-07 00:07:53 -0600

    Project deleted

--- ios/Utility.swift
+++
@@ -1,8 +0,0 @@
-@objc(Utility)
-class Utility: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
