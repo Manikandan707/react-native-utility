39796acefb1ae591e0cb051b969e80052cf33f76
Manikandan <manikandan707@gmail.com>
2024-02-06 23:19:43 -0600

    deleted the project

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  UtilityExample
-//
-
-import Foundation
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type UtilityType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { Utility } = NativeModules;
-
-export default Utility as UtilityType;
